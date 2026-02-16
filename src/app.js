a2565a0296102f3afce1f2a741294fa175553548
blackshark281 <ritikgupta281@gmail.com>
2026-02-16 22:39:43 +0530

    practicing routes

--- src/app.js
+++ src/app.js
@@ -2,13 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use("/local", (req, res)=>{
-    res.send("Hello local 1");
+app.get("/local/:userId/:name", (req, res)=>{
+    console.log(req.params);
+    res.send("Hello local GET");
+})
+
+app.get("/local", (req, res)=>{
+    console.log(req.query);
+    res.send("Hello local GET");
+})
+
+app.post("/local", (req, res)=>{
+    res.send("Hello local POST");
 })
 
 app.use("/", (req, res)=>{
     res.send("Hello global");
 })
+
 
 app.listen(3000, (req, res) =>{
     console.log("Server started on port 3000");
